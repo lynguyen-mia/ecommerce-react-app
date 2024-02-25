78e39a1234235ab17175e3fc222b185e9206fc06
Ly Nguyen <nguyenthihuongly21@gmail.com>
2024-02-25 20:25:39 +0700

    changes

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -4,20 +4,12 @@ import LiveChat from "../components/livechat/LiveChat";
 import { getVariable } from "../utils/getLocalVars";
 
 const ErrorPage = () => {
-  const user = getVariable("user");
   return (
     <>
       <div className="container" style={{ maxWidth: "1140px" }}>
         <NavBar />
       </div>
-      {user ? (
-        <h3 className="text-center my-5">Page Not Found.</h3>
-      ) : (
-        <div className="text-center mt-4 fs-5">
-          <i className="bi bi-x-circle-fill"></i> Session timed out!
-          <div>Please log in again</div>
-        </div>
-      )}
+      <h3 className="text-center my-5">Page Not Found.</h3>
       <Footer />
       <LiveChat />
     </>
