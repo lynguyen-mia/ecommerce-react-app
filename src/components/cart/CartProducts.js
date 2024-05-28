38cc4b9988cb7477c4e7f42a132c668550e63b0b
ly_nguyen <nguyenthihuongly21@gmail.com>
2024-05-28 20:09:33 +0700

    debug cart

--- src/components/cart/CartProducts.js
+++ src/components/cart/CartProducts.js
@@ -79,7 +79,7 @@ const CartProducts = ({ listcart, setCartProducts }) => {
             <tbody>
               {/* For each product, create a row */}
               {listcart.map((p) => {
-                console.log(listcart);
+                if (!p.product) return;
                 const total = Number(p.quantity) * Number(p.product?.price);
                 return (
                   <tr key={p.product._id} className="row">
