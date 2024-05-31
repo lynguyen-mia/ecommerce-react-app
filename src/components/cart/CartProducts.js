26bf646f9fc98f4d47d1bb99299f564d59979f54
ly_nguyen <nguyenthihuongly21@gmail.com>
2024-05-31 12:25:08 +0700

    fix cart bugs

--- src/components/cart/CartProducts.js
+++ src/components/cart/CartProducts.js
@@ -1,158 +1,145 @@
-import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import convertToVND from "../../utils/convertToVND";
-import ChangeQuantityBtn from "./ChangeQuantityBtn";
+import { useReducer, useState } from "react";
 import styles from "./CartProducts.module.css";
 
-const CartProducts = ({ listcart, setCartProducts }) => {
-  const [deleteAlert, setDeteleAlert] = useState(false);
+const ChangeQuantityBtn = ({ quantity, product, changeCartProducts }) => {
+  const [quantityNum, setQuantityNum] = useState(quantity);
 
-  // Delete product
-  async function onClickRemove(id) {
-    try {
-      const confirm = window.confirm("Are you sure to delete this product?");
-      if (confirm) {
-        const res = await fetch(
-          `https://ecommerce-node-app-sfau.onrender.com/client/cart-remove?id=${id}`,
-          {
-            credentials: "include",
-          }
-        );
+  // useReducer
+  const alertReducer = (state, action) => {
+    switch (action.type) {
+      case "add":
+        return [...state, action.item];
+      case "remove":
+        return state.slice(1);
+      default:
+        return state;
+    }
+  };
 
-        if (res.status === 401) {
-          window.alert("Your session has expired, please log in again");
-          return window.location.replace("/login");
-        }
+  const [orderAlert, dispatchAlert] = useReducer(alertReducer, []);
 
-        if (res.ok) {
-          // Refresh cart products
-          const result = await res.json();
-          setCartProducts(result.data);
-          // Display delete alert
-          setDeteleAlert(true);
-          setTimeout(() => setDeteleAlert(false), 1500);
-        }
+  async function updateQuantity(prodId, quantity) {
+    const res = await fetch(
+      "https://ecommerce-node-app-sfau.onrender.com/client/edit-cart",
+      {
+        method: "POST",
+        credentials: "include",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ prodId: prodId, quantity: quantity }),
       }
-    } catch (err) {
-      console.log(err);
+    );
+
+    if (res.status === 401) {
+      window.alert("Your session has expired, please log in again");
+      return window.location.replace("/login");
+    }
+
+    const result = await res.json();
+
+    if (res.status === 404) {
+      setQuantityNum((prev) => Number(result.stock));
+      window.alert(`The maximum quantity in stock now is ${result.stock}`);
+    }
+
+    changeCartProducts(result.data);
+  }
+
+  const dispatchAlertFn = () => {
+    dispatchAlert({ type: "add", item: "Bạn đã sửa hàng thành công!" });
+    setTimeout(() => {
+      dispatchAlert({ type: "remove" });
+    }, 1500);
+  };
+
+  // Edit quantity
+  function onEditQuantity(e) {
+    const quantity = Number(e.target.value);
+    setQuantityNum(quantity);
+    if (quantity > 0 && quantity < 1000) {
+      updateQuantity(product._id, quantity);
+      dispatchAlertFn();
     }
   }
 
-  const navigate = useNavigate();
-  function onClickShopping() {
-    navigate("/shop");
-    window.scroll({ top: 0, left: 0, behavior: "instant" });
+  // Decrement, Increment quantity
+  function onDecrement() {
+    const totalQuantity = Number(quantityNum) - 1;
+    if (totalQuantity > 0 && totalQuantity < 1000) {
+      setQuantityNum(totalQuantity);
+      updateQuantity(product._id, totalQuantity);
+      dispatchAlertFn();
+    }
   }
 
-  function onClickCheckout() {
-    navigate("/checkout");
-    window.scroll({ top: 0, left: 0, behavior: "instant" });
+  function onIncrement() {
+    const totalQuantity = Number(quantityNum) + 1;
+    if (totalQuantity > 0 && totalQuantity < 1000) {
+      setQuantityNum(totalQuantity);
+      updateQuantity(product._id, totalQuantity);
+      dispatchAlertFn();
+    }
+  }
+
+  // Prevent users from entering quantity less than 1 and larger than 999
+  function onHandleInvalid(e) {
+    if (Number(e.target.value) < 1) {
+      e.target.value = 1;
+      setQuantityNum(1);
+      dispatchAlertFn();
+      return;
+    }
+    if (Number(e.target.value) > 999) {
+      e.target.value = 999;
+      setQuantityNum(999);
+      dispatchAlertFn();
+      return;
+    }
   }
 
   return (
-    <div id={styles["cart-products"]} className="d-flex flex-column gap-3">
-      <div className={`table-responsive ${styles.table}`}>
-        {/* PRODUCT TABLE */}
-        <table className="table table-borderless text-center align-middle">
-          <thead className={styles["product-header"]}>
-            <tr className="row">
-              <th className="col">IMAGE</th>
-              <th className="col col-3">PRODUCT</th>
-              <th className="col">PRICE</th>
-              <th className="col">QUANTITY</th>
-              <th className="col">TOTAL</th>
-              <th className="col">REMOVE</th>
-            </tr>
-          </thead>
-
-          {listcart && listcart.length === 0 && (
-            <tbody>
-              <tr>
-                <td colSpan={12} className="pt-4 text-secondary">
-                  No product in cart.
-                </td>
-              </tr>
-            </tbody>
-          )}
-          {listcart && listcart.length > 0 && (
-            <tbody>
-              {/* For each product, create a row */}
-              {listcart.map((p) => {
-                const total = Number(p.quantity) * Number(p.product?.price);
-                return (
-                  <tr key={p.product?._id} className="row">
-                    {/* Images column*/}
-                    <td className="col d-flex justify-content-center">
-                      <img src={p.product?.img1} alt={p.product?.name} />
-                    </td>
-                    {/* Names column */}
-                    <td className="col col-3">
-                      <Link
-                        to={`/detail/${p.product?._id}`}
-                        className={styles["product-name"]}
-                      >
-                        {p.product?.name}
-                      </Link>
-                    </td>
-                    {/* Prices column */}
-                    <td className="col">
-                      <div className={styles.number}>
-                        {convertToVND(p.product?.price)}
-                      </div>
-                    </td>
-                    {/* Quantity column */}
-                    <td className="col">
-                      <ChangeQuantityBtn
-                        quantity={p.quantity}
-                        product={p.product}
-                        changeCartProducts={setCartProducts}
-                      />
-                    </td>
-                    {/* Total column */}
-                    <td className="col">
-                      <div className={styles.number}>{convertToVND(total)}</div>
-                    </td>
-                    {/* Remove column */}
-                    <td className="col">
-                      <button
-                        className="border-0 bg-transparent"
-                        onClick={() => onClickRemove(p.product["_id"])}
-                      >
-                        <i className="bi bi-trash3"></i>
-                      </button>
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          )}
-        </table>
-      </div>
-
-      {/* Delete alert */}
-      {deleteAlert && (
-        <div className={styles["delete-alert"]}>Xoá phẩm khỏi giỏ hàng</div>
-      )}
-
-      {/* Buttons */}
-      <div
-        className={`${styles.actions} d-flex flex-md-row flex-column justify-content-between w-80 mb-4`}
+    <div className={`${styles["product-column"]}`}>
+      <button
+        className={`${styles["input-button"]} ${
+          quantityNum <= 1 ? styles.disabled : ""
+        } `}
+        onClick={onDecrement}
+        disabled={quantityNum <= 1 ? true : false}
       >
-        <button
-          className="btn border-0 text-secondary"
-          onClick={onClickShopping}
-        >
-          <i className="bi bi-arrow-left"></i> Continue shopping
-        </button>
-        <button
-          className="btn text-secondary border-dark-subtle"
-          onClick={onClickCheckout}
-        >
-          Proceed to checkout <i className="bi bi-arrow-right fw-bold"></i>
-        </button>
+        <i className="bi bi-caret-left-fill"></i>
+      </button>
+      <input
+        type="number"
+        className="border-0"
+        name="quantity"
+        placeholder="1"
+        step="1"
+        min="1"
+        max="999"
+        value={quantityNum}
+        onBlur={(e) => onHandleInvalid(e)}
+        onChange={(e) => onEditQuantity(e)}
+        style={{ fontSize: "15px", fontStyle: "italic" }}
+      />
+      <button
+        className={`${styles["input-button"]} ${
+          quantityNum >= 999 ? styles.disabled : ""
+        } pe-3`}
+        onClick={onIncrement}
+        disabled={quantityNum >= 999 ? true : false}
+      >
+        <i className="bi bi-caret-right-fill"></i>
+      </button>
+
+      {/* Change order alert */}
+      <div className={styles["alert-container"]}>
+        {orderAlert.map((alert, index) => (
+          <div key={index} className={styles["order-alert"]}>
+            {alert}
+          </div>
+        ))}
       </div>
     </div>
   );
 };
 
-export default CartProducts;
+export default ChangeQuantityBtn;
